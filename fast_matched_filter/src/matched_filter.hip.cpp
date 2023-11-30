93a5b6ea8285b3744f2736e27cc19549883827eb
Jean-Yves Vet <contact@jean-yves.vet>
2023-11-30 13:53:55 +0100

    using streams on blocks instead of templates

--- fast_matched_filter/src/matched_filter.hip.cpp
+++ fast_matched_filter/src/matched_filter.hip.cpp
@@ -230,6 +230,7 @@ extern "C"
         const size_t count_template = (n_samples_template / WARPSIZE + 1) * WARPSIZE;
         const size_t count_data = ((n_samples_template + BLOCKSIZE * step) / WARPSIZE + 1) * WARPSIZE;
         const size_t sharedMem = (count_template + count_data + 1) * sizeof(float);
+        const int maxSharedMem = props.sharedMemPerBlock;
         if (sharedMem > maxSharedMem)
         {
             size_t new_step = (maxSharedMem / sizeof(float) - 2 * n_samples_template - 2 * WARPSIZE) / BLOCKSIZE;
@@ -245,7 +246,6 @@ extern "C"
             exit(0);
         }
 
-        hipStream_t *streams = (hipStream_t*) malloc(n_templates * sizeof(hipStream_t));
         // loop over templates
         for (size_t t = 0; t < n_templates; t++)
         {
@@ -255,27 +255,25 @@ extern "C"
             int *moveouts_d = NULL;
             float *sum_square_templates_d = NULL;
             float *weights_d = NULL;
-
-            hipStreamCreateWithFlags(&streams[t], hipStreamNonBlocking);
                 
-            hipMallocAsync((void **)&cc_mat_d, sizeof_cc_mat, streams[t]);
-            hipMemsetAsync(cc_mat_d, 0, sizeof_cc_mat, streams[t]);
+            hipMallocAsync((void **)&cc_mat_d, sizeof_cc_mat, NULL);
+            hipMemsetAsync(cc_mat_d, 0, sizeof_cc_mat, NULL);
 
-            hipMallocAsync((void **)&cc_out_d, sizeof_cc_out, streams[t]);
+            hipMallocAsync((void **)&cc_out_d, sizeof_cc_out, NULL);
 
             size_t bytes = n_stations * n_components * sizeof(float);
-            hipMallocAsync((void **)&sum_square_templates_d, bytes, streams[t]);
-            hipMemcpyAsync(sum_square_templates_d, &sum_square_templates[t * n_stations * n_components], bytes, hipMemcpyHostToDevice, streams[t]);
+            hipMallocAsync((void **)&sum_square_templates_d, bytes, NULL);
+            hipMemcpyAsync(sum_square_templates_d, &sum_square_templates[t * n_stations * n_components], bytes, hipMemcpyHostToDevice, NULL);
 
-            hipMallocAsync((void **)&moveouts_d, bytes, streams[t]);
-            hipMemcpyAsync(moveouts_d, &moveouts[t * n_stations * n_components], bytes, hipMemcpyHostToDevice, streams[t]);
+            hipMallocAsync((void **)&moveouts_d, bytes, NULL);
+            hipMemcpyAsync(moveouts_d, &moveouts[t * n_stations * n_components], bytes, hipMemcpyHostToDevice, NULL);
 
-            hipMallocAsync((void **)&weights_d, bytes, streams[t]);
-            hipMemcpyAsync(weights_d, &weights[t * n_stations * n_components], bytes, hipMemcpyHostToDevice, streams[t]);
+            hipMallocAsync((void **)&weights_d, bytes, NULL);
+            hipMemcpyAsync(weights_d, &weights[t * n_stations * n_components], bytes, hipMemcpyHostToDevice, NULL);
 
             bytes *= n_samples_template;
-            hipMallocAsync((void **)&templates_d, bytes, streams[t]);
-            hipMemcpyAsync(templates_d, &templates[t * n_samples_template * n_stations * n_components], bytes, hipMemcpyHostToDevice, streams[t]);
+            hipMallocAsync((void **)&templates_d, bytes, NULL);
+            hipMemcpyAsync(templates_d, &templates[t * n_samples_template * n_stations * n_components], bytes, hipMemcpyHostToDevice, NULL);
 
             size_t n_corr_t;
             int max_moveout;
@@ -284,7 +282,6 @@ extern "C"
             float *cc_out_t = NULL;
             float *sum_square_templates_d_t = NULL;
             float *weights_d_t = NULL;
-            int maxSharedMem = props.sharedMemPerBlock;
 
             // compute the number of correlation steps for this template
             moveouts_t = moveouts + t * n_stations * n_components;
@@ -297,9 +294,14 @@ extern "C"
             }
             n_corr_t = (n_samples_data - n_samples_template - max_moveout) / step + 1;
 
+            hipDeviceSynchronize();
+
+            hipStream_t streams[NCHUNKS];
             //#pragma omp parallel for
             for (size_t ch = 0; ch < NCHUNKS; ch++)
             {
+                hipStreamCreateWithFlags(&streams[ch], hipStreamNonBlocking);
+
                 size_t chunk_offset = ch * chunk_size;
                 size_t cs;
                 // make sure the chunk is not going out of bounds
@@ -319,7 +321,7 @@ extern "C"
                 dim3 GS(ceilf(cs / (float)BS.x) * n_stations);
 
                     // process
-                hipLaunchKernelGGL(network_corr, dim3(GS), dim3(BS), sharedMem, streams[t], templates_d,
+                hipLaunchKernelGGL(network_corr, dim3(GS), dim3(BS), sharedMem, streams[ch], templates_d,
                                                     sum_square_templates_d,
                                                     moveouts_d,
                                                     data_d,
@@ -340,12 +342,12 @@ extern "C"
                 if (sum_cc_mode > 0)
                 {
                     // weighted sum of correlation coefficients
-                    hipMemsetAsync(cc_out_d, 0, sizeof_cc_out, streams[t]);
+                    hipMemsetAsync(cc_out_d, 0, sizeof_cc_out, streams[ch]);
 
                     // using a small block size seems to improve the speed of sum_cc
                     dim3 BS_sum(32);
                     dim3 GS_sum(ceilf(cs / (float)BS_sum.x));
-                    hipLaunchKernelGGL(sum_cc, dim3(GS_sum), dim3(BS_sum), 0, streams[t], cc_mat_d, cc_out_d, weights_d,
+                    hipLaunchKernelGGL(sum_cc, dim3(GS_sum), dim3(BS_sum), 0, streams[ch], cc_mat_d, cc_out_d, weights_d,
                                                n_stations, n_components,
                                                n_corr_t, chunk_offset, cs);
 
@@ -355,29 +357,29 @@ extern "C"
                     // xfer cc_sum back to host
                     sizeof_cc_out_chunk = sizeof(float) * cs;
                     cc_out_t = cc_out + t * n_corr + chunk_offset;
-                    hipMemcpyAsync(cc_out_t, cc_out_d, sizeof_cc_out_chunk, hipMemcpyDeviceToHost, streams[t]);
+                    hipMemcpyAsync(cc_out_t, cc_out_d, sizeof_cc_out_chunk, hipMemcpyDeviceToHost, streams[ch]);
                 }
                 else
                 {
                     // xfer cc_mat back to host
                     sizeof_cc_out_chunk = sizeof(float) * cs * n_stations * n_components;
                     cc_out_t = cc_out + (t * n_corr + chunk_offset) * n_stations * n_components;
-                    hipMemcpyAsync(cc_out_t, cc_mat_d, sizeof_cc_out_chunk, hipMemcpyDeviceToHost, streams[t]);
+                    hipMemcpyAsync(cc_out_t, cc_mat_d, sizeof_cc_out_chunk, hipMemcpyDeviceToHost, streams[ch]);
                 }
             }
 
-            hipFreeAsync(cc_mat_d, streams[t]);
-            hipFreeAsync(cc_out_d, streams[t]);
-            hipFreeAsync(sum_square_templates_d, streams[t]);
-            hipFreeAsync(weights_d, streams[t]);
-            hipFreeAsync(templates_d, streams[t]);
-            hipFreeAsync(moveouts_d, streams[t]);
+            hipDeviceSynchronize();
+            for (size_t ch = 0; ch < NCHUNKS; ch++)
+                hipStreamDestroy(streams[ch]);
+
+            hipFreeAsync(cc_mat_d, NULL);
+            hipFreeAsync(cc_out_d, NULL);
+            hipFreeAsync(sum_square_templates_d, NULL);
+            hipFreeAsync(weights_d, NULL);
+            hipFreeAsync(templates_d, NULL);
+            hipFreeAsync(moveouts_d, NULL);
         }
 
-        hipDeviceSynchronize();
-
         hipFree(data_d);
-
-        free(streams);
     }     //  matched_filter
 } // extern C
